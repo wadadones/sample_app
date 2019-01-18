c51bbfcae8836a46f54dc259b9ee3879fd345e88
n.wada <jones3429@outlook.jp>
2019-01-18 14:38:19 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-	get 'static_pages/help'
-	get 'static_pages/about'
-	get 'static_pages/contact'
-  root 'static_pages#home'
+  get '/signup', to: 'users#new'
 
+  get '/help', to: 'static_pages#help'
+  #getリクエストが/helpに送られたとき、StaticPageコントローラのhelpアクションが呼ばれる
+  #help_path -> '/help'
+  #help_url -> 'http://www.example.com/help'が利用可能になる
+	get '/about', to: 'static_pages#about'
+	get '/contact', to: 'static_pages#contact'
+  root 'static_pages#home'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
