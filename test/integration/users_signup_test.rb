2ab0573221edd01c2a0223fad73916acacf0755f
n.wada <jones3429@outlook.jp>
2019-01-24 02:18:55 +0900

    Add account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
   test "invalid signup information" do
   	get signup_path
   	assert_no_difference 'User.count' do
@@ -16,7 +18,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   	assert_select 'div.alert'
   end
 
-  test "valid signup information" do
+
+
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Example User",
@@ -25,6 +29,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                         password_confirmation: "password"} }
 
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size  # 配信メッセージは一つか？
+    user = assigns(:user)
+    assert_not user.activated?
+    #有効かしていない場合でのログイン
+    log_in_as(user)
+    assert_not is_logged_in?
+    #有効化トークンが不正の場合
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    #トークン正しいがめーるあどれすが無効な場合
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    #有効化トークンが正しい場合
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.empty?
