7756034dc1e2dffcb5d8595655854a35354db835
n.wada <jones3429@outlook.jp>
2019-01-24 16:04:31 +0900

    modify host in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://morning-headland-91413.herokuapp.com'
+  host = 'https://aqueous-beyond-94595.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
